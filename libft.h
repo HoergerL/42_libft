97ddf5e65303f633f3dafe328b57a96752dfe130
Laura Hoerger <lhoerger@1-B-3.42heilbronn.de>
2021-06-22 17:23:41 +0200

    ft_fd

--- libft.h
+++ libft.h
@@ -35,4 +35,11 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char *ft_strtrim(char const *s1, char const *set);
+char *ft_itoa(int n);
+char	**ft_split(char const *s, char c);
+
+void	ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
 #endif
