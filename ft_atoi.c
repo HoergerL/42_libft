3f30a5d384bfa6416ac8ac02837aa9897bde6c6a
Laura Hörger <66875014+HoergerL@users.noreply.github.com>
2021-08-17 15:39:13 +0200

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,6 +12,11 @@
 
 #include "libft.h"
 
+/* 
+This function checks for minuses and mistakes in the string. 
+The number read by print_numb() will be mulitplied by the return value of this function.
+@return: So if there is a minus, it will return -1, if there is an error it will return 0 and if there is a plus the result will be 1.
+*/
 static int	check_minus(const char *str, int len)
 {
 	int	cnt;
@@ -31,6 +36,10 @@ static int	check_minus(const char *str, int len)
 	return (0);
 }
 
+/* 
+This function translates the string into an integer
+@return: It will return the number independent of the sign
+*/
 static int	print_numb(const char *str)
 {
 	int	cnt;
@@ -49,7 +58,10 @@ static int	print_numb(const char *str)
 	}
 	return (nb);
 }
-
+/* 
+This function reads in a String, starting with whitespaces and either a plus or a minus followed by a number.
+@return: The function will return a number with the correct sign, or 0 if the syntax of the function wasn't correct
+*/
 int	ft_atoi(const char *str)
 {
 	int	minus;
